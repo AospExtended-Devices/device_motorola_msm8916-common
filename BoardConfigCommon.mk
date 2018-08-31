8bf6672a63f2d56e0d1ef22ed5e2f6727c7fcb0d
Danny Baumann <dannybaumann@web.de>
2018-08-31 13:35:20 +0200

    msm8916-common: Mark camera-related processes as API level 23.
    Our camera blobs trigger some checks for broken behavior (destroyed
    mutex usage, invalid pthread usage) present in newer API levels.
    
    Change-Id: I8705437e0a5c6d80b86748bff559ce1e8a7bf716

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,6 +78,8 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_PROVIDES_CAMERA_HAL := true
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 TARGET_USES_MEDIA_EXTENSIONS := true
+TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
+	/system/vendor/bin/mm-qcamera-daemon=23
 
 # Charger
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
